72e0c7b7eeadffaea726e29b9d8fb8065e69dfd6
Miriam Fraga Rodríguez <fraga@MacBook-Pro-de-Miriam.local>
2023-03-20 17:36:53 +0100

    modified api url's

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import './App.scss';
+import FilmsPage from './pages/FilmsPages/FilmsPage';
 
 function App() {
   return (
     <div className="App">
+    <FilmsPage></FilmsPage>
     </div>
   );
 }
--- src/pages/FilmsPages/FilmsPage.js
+++ src/pages/FilmsPages/FilmsPage.js
@@ -1,9 +1,9 @@
 import React from 'react'
-
+import FilmsComponent from '../../components/FilmsComponent/FilmsComponent';
 const FilmsPage = () => {
   return (
     <div>
-      
+      <FilmsComponent></FilmsComponent>
     </div>
   )
 }
