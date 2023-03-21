1d2e01da1db4984747ad8e910dbe7a014f92fa2d
Miriam Fraga Rodríguez <fraga@MacBook-Pro-de-Miriam.local>
2023-03-21 13:29:06 +0100

    first images and poster images, without scss

--- src/components/FilmsComponent/FilmsComponent.js
+++ src/components/FilmsComponent/FilmsComponent.js
@@ -27,8 +27,11 @@ if(loadingFilms){
         <h1>titulo:{f.original_title}</h1>
         <h1>titulo:{f.release_date}</h1>
         <h3>reseña:{f.overview}</h3>
-        <img src={f.backdrop_path} alt={f.title}></img>
-        <img src={f.poster_path} alt={f.title}></img>
+        {/* si en vez de poner https://image.tmdb.org/t/p/w500/ ponemos https://image.tmdb.org/t/p/original/ */}
+        <img src={`https://image.tmdb.org/t/p/w500/${f.backdrop_path}`} alt={f.title}></img>
+        <img src={`https://image.tmdb.org/t/p/w500/${f.poster_path}`} alt={f.title}></img>
+        {console.log(f.poster_path)}
+        {console.log(f)}
         <h3>popularity:{f.popularity}</h3>
         {f.genre_ids.map((x,index)=>{
 return (
