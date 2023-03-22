3f1a5d1b0e6ebff3f357f388bf268cb639c80c83
Miriam Fraga Rodríguez <fraga@MacBook-Pro-de-Miriam.local>
2023-03-22 07:26:47 +0100

    first files about videoFilmsComponent

---
+++ src/components/VideoFilmsComponent/VideoFilmsComponent.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './VideoFilmsComponent.scss';
+
+const VideoFilmsComponent = () => { 
+ return(  <div className={styles.VideoFilmsComponent}>
+    VideoFilmsComponent Component
+  </div>)
+};
+
+VideoFilmsComponent.propTypes = {};
+
+VideoFilmsComponent.defaultProps = {};
+
+export default VideoFilmsComponent;
---
+++ src/store/videos/actions.js
@@ -0,0 +1,38 @@
+import axios from "axios";
+import { GET_VIDEOS, GET_VIDEOS_OK,GET_VIDEOS_FAIL } from "./actionTypes";
+
+export function actionGetVideos(){
+    return{
+        type: GET_VIDEOS
+        
+    }
+}
+export function actionGetVideosOk(videos){
+    return{
+        type: GET_VIDEOS_OK,
+        payload: videos
+    }
+}
+export function actionGetVideosFail(error){
+    return{
+        type: GET_VIDEOS_FAIL,
+        payload: error
+    }
+}
+
+export function getVideos(){
+    return async (dispatch)=>{
+        dispatch(actionGetVideos())
+        try {
+            const videos = [];
+            for (let i=0; i<20; i++){
+                const response = await axios.get(`https://api.themoviedb.org/${i}/movie/videos?api_key=84d794a5b6f706450c3ee085b451575c&append_to_response=videos`)
+                 videos.push(response.data)
+            }
+            dispatch(actionGetVideosOk(videos))
+        } catch (error) {
+            dispatch(actionGetVideosFail(error))
+        }
+
+    }
+}
