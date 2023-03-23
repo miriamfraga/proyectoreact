a877c0ecea18c7feaf4f20955fab093f299a6b61
Miriam Fraga Rodríguez <fraga@MacBook-Pro-de-Miriam.local>
2023-03-23 12:44:46 +0100

    merging...

--- src/store/films/actions.js
+++ src/store/films/actions.js
@@ -126,24 +126,7 @@ export function getFilmsDoc(){
        }
       
     }
-
-export function getFilms(){
-    return async(dispatch) => {
-        dispatch(actionGetFilms())
-        try {
-            const films = [];
-            for (let i=1; i<20; i++){
-                const response = await axios.get(`https://api.themoviedb.org/3/discover/movie?api_key=84d794a5b6f706450c3ee085b451575c&page=${i}`);
-                films.push(response.data.results)
-                console.log(response.data.page,"response.data")
-            }
-            dispatch(actionGetFilmsOk(films))
-            // console.log(response.data)
-        } catch (error) {
-            dispatch(actionGetFilmsFail(error))
-        }
-     }
-}
+ }
 
 /* GET POPULAR FILMS */
 
