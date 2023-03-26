adeb446de313b422d6754559d532e881eac32cc7
Miriam Fraga Rodríguez <fraga@MacBook-Pro-de-Miriam.local>
2023-03-26 19:29:02 +0200

    save ordered html

--- src/components/SingleFilmComponent/SingleFilmComponent.js
+++ src/components/SingleFilmComponent/SingleFilmComponent.js
@@ -2,8 +2,9 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import  './SingleFilmComponent.scss';
 import { useSelector } from 'react-redux';
-
+import { useState } from 'react';
 const SingleFilmComponent = () => { 
+
 
   const {film, loadingSingle}= useSelector((state)=>state.FilmsReducer)
   if(loadingSingle){
@@ -11,23 +12,30 @@ const SingleFilmComponent = () => {
       <p>"Loading..."</p>
     )
   }
+
+      //  IMAGEN APAISADA DE LA PELI
+     //  POPULARIDAD DE LA PELI
+     //  IMAGEN POSTER 
+     //    TITULO DE LA PELI 
+     //   RESEÑA DE LA PELI       
+     //   AÑO DE LANZAMIENTO 
   const urlImage = "https://image.tmdb.org/t/p/w500/"
   return(<section className="section__detail">
-    <div className='section__detail__div'>
-  {/* {console.log(film, "film")} */}
-      <h3 className="section__detail__div__wrapper__title">{film.popularity}</h3>
-   
-      <img className="section__detail__div__img__bg" src={`${urlImage}${film.poster_path}`} alt={film.title}></img>
-      <img  className="section__detail__div__img__pstr" src={`${urlImage}${film.backdrop_path}`} alt={film.title}></img>
 
+        <div className='section__detail__div'>
+           <h3 className="section__detail__div__wrapper__title">{film.popularity}</h3>
      
-      <h2 className="section__detail__div__wrapper__title">{film.original_title}  {film.genres.map((genre)=> <div key={genre.id}> 
-      <h4 key={genre.id} > { genre.name } </h4> </div> )}</h2>
-      
-      <h4 className="section__detail__div__wrapper__ovvw">{film.overview} </h4>
-      <h3 className="section__detail__div__wrapper__title">{film.release_date}</h3>
-       
-      
+           <img className="section__detail__div__img__bg" src={`${urlImage}${film.poster_path}`} alt={film.title}></img>
+           <img  className="section__detail__div__img__pstr" src={`${urlImage}${film.backdrop_path}`} alt={film.title}></img>
+           <h2 className="section__detail__div__wrapper__title"> {film.original_title}  </h2>
+           <h4 className="section__detail__div__wrapper__ovvw">{film.overview} </h4>
+           <h3 className="section__detail__div__wrapper__title">{film.release_date}</h3>
+
+      {/* // -- AQUÍ ESTAN LOS NOMBRES DE LOS GÉNEROS EN UN ARRAY -- */}
+      {/* {film.genres.map((genre)=> <div key={genre.id}>  */}
+      {/* <h4 key={genre.id} > { genre.name } </h4> </div> )} */}
+     
+  
      {/* { -- METER EL ICONO DE REPRODUCIR EL VÍDEO } */}
       <button className="section__detail__div__button__watch">Ver ahora</button>  <button className="section__detail__div__button__watch">*</button>      <button className="section__detail__div__button__watch">*</button>  
           </div>
