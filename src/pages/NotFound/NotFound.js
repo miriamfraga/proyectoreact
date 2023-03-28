43243b6c5340a9fc916d9979b1916cdb5765c0bd
Miriam Fraga Rodríguez <fraga@MacBook-Pro-de-Miriam.local>
2023-03-28 16:09:14 +0200

    notFound

--- src/pages/NotFound/NotFound.js
+++ src/pages/NotFound/NotFound.js
@@ -1,11 +1,21 @@
 import React from 'react'
-
+import './NotFound.scss';
+import medusa0 from '../../assets/images/medusa0.jpeg'
 const NotFound = () => {
+
+  setTimeout(()=>{
+    NotFound()
+  }, 3000)
   return (
-    <div>
-    <h1>YOU SEEM TO BE LOST</h1>
+    <div className='div__notFound'>
+    <h2>Turned into stone.</h2>
+    <h1>404</h1>
+
+    <img className="div__notFound__img" src={medusa0} alt="medusa not found"></img>
     </div>
   )
+
 }
+
 
 export default NotFound
